86ed9635fb50f4b285622edfb2ddbb06e9de0706
briandenicola <brian@briandenicola.com>
2025-04-22 15:31:32 -0500

    🏓 Tue Apr 22 15:31:32 CDT 2025

--- infrastructure/kubernetes/main.tf
+++ infrastructure/kubernetes/main.tf
@@ -14,7 +14,7 @@ locals {
   sql_name                 = "${local.resource_name}-sql"
   static_webapp_location   = "centralus"
   app_path                 = "./cluster-config"
-  flux_repository          = "https://github.com/briandenicola/openai-coin-analyzer"
+  flux_repository          = "https://github.com/briandenicola/dapr-ai-agents-demo"
   environment_type         = "dev"
   vnet_cidr                = cidrsubnet("10.0.0.0/8", 8, random_integer.vnet_cidr.result)
   pe_subnet_cidir          = cidrsubnet(local.vnet_cidr, 8, 1)
@@ -22,12 +22,11 @@ locals {
   nodes_subnet_cidir       = cidrsubnet(local.vnet_cidr, 8, 3)
   compute_subnet_cidir     = cidrsubnet(local.vnet_cidr, 8, 4)
   sql_subnet_cidir         = cidrsubnet(local.vnet_cidr, 8, 10)
-  kubernetes_version       = "1.31"
+  kubernetes_version       = "1.32"
   istio_version            = "asm-1-24"
   home_ip_address          = chomp(data.http.myip.response_body)
   apim_backend_name        = "${local.resource_name}-backend"
   apim_api_path            = "api"
-  swagger_url              = "https://raw.githubusercontent.com/briandenicola/openai-coin-analyzer/refs/heads/main/docs/swagger.json"
   app_apim_gateway_url     = "${azurerm_api_management.this.gateway_url}/${local.apim_api_path}/analyze"
   otel_namespace           = "otel-system"
   postgresql_user_name     = "manager"
