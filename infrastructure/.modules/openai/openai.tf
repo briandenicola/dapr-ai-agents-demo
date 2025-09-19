858266f4c15b32b83921ede90d77550439ca742a
briandenicola <brian@briandenicola.com>
2025-09-19 10:59:01 -0500

    ⚾ Fri Sep 19 10:59:01 CDT 2025

--- infrastructure/.modules/openai/openai.tf
+++ infrastructure/.modules/openai/openai.tf
@@ -2,13 +2,38 @@ data "azurerm_monitor_diagnostic_categories" "this" {
   resource_id = azurerm_cognitive_account.this.id
 }
 
-resource "azurerm_cognitive_account" "this" {
-  name                  = local.openai_name
-  resource_group_name   = var.resource_group.name
-  location              = var.resource_group.location
-  kind                  = "OpenAI"
-  custom_subdomain_name = local.openai_name
-  sku_name              = "S0"
+# resource "azurerm_cognitive_account" "this" {
+#   name                  = local.openai_name
+#   resource_group_name   = var.resource_group.name
+#   location              = var.resource_group.location
+#   kind                  = "AIServices"
+#   custom_subdomain_name = local.openai_name
+#   sku_name              = "S0"
+# }
+
+resource "azapi_resource" "global" {
+  type      = "Microsoft.CognitiveServices/account@2025-06-01"
+  name      = "${local.openai_name}-global"
+  location  = var.resource_group.location
+  parent_id = var.resource_group.id
+
+  body = {
+    sku = {
+      name = "S0"
+    },
+    kind = "AIServices",
+    properties = {
+      allowProjectManagement = true
+      customSubDomainName =  local.openai_name
+      publicNetworkAccess = "Enabled"
+    }
+  }
+}
+
+data "azurerm_cognitive_account" "global" {
+  depends_on          = [azapi_resource.global]
+  name                = "${local.openai_name}-global"
+  resource_group_name = var.resource_group.name
 }
 
 resource "azurerm_monitor_diagnostic_setting" "this" {
@@ -19,7 +44,7 @@ resource "azurerm_monitor_diagnostic_setting" "this" {
   ]
 
   name                       = "diag"
-  target_resource_id         = azurerm_cognitive_account.this.id
+  target_resource_id         = data.azurerm_cognitive_account.this.id
   log_analytics_workspace_id = var.log_analytics.workspace_id
 
   dynamic "enabled_log" {
--- infrastructure/.modules/openai/outputs.tf
+++ infrastructure/.modules/openai/outputs.tf
@@ -1,19 +1,19 @@
 output "OPENAI_RESOURCE_ID" {
-  value     = azurerm_cognitive_account.this.id
+  value     = data.azurerm_cognitive_account.this.id
   sensitive = false
 }
 
 output "OPENAI_ENDPOINT" {
-  value     = azurerm_cognitive_account.this.endpoint
+  value     = data.azurerm_cognitive_account.this.endpoint
   sensitive = false
 }
 
 output "OPENAI_RESOURCE_NAME" {
-  value     = azurerm_cognitive_account.this.name
+  value     = data.azurerm_cognitive_account.this.name
   sensitive = false
 }
 
 output "OPENAI_PRIMARY_KEY" {
-  value     = azurerm_cognitive_account.this.primary_access_key
+  value     = data.azurerm_cognitive_account.this.primary_access_key
   sensitive = true
 }
